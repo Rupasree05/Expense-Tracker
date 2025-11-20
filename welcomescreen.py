011e53e89d8e6c40bcb560935924fa55f984c25c
rupasreesaladi <rupasreesaladi@gmail.com>
2025-11-20 20:44:39 +0530

    Update welcomescreen.py

--- welcomescreen.py
+++ welcomescreen.py
@@ -1,19 +1,22 @@
 import streamlit as st
-import streamlit as st
 
 st.set_page_config(page_title="Smart Expense", layout="centered")
-st.image("C:/Users/User/Desktop/Rupa Sree/expense_Tracker_Image.png")
 
-# Center content
+# Show your custom image
+st.image("C:/Users/User/Desktop/Rupa Sree/expense_Tracker_Image.png", 
+         width=250)
 
+# Text content
 st.markdown("""
-<h1 style="color:#2C3E50; font-size: 40px;">Smart Expense</h1>
-<p style="font-size:18px; color:#555;">
+<h1 style="color:#2C3E50; font-size: 40px; text-align:center;">
+    Smart Expense
+</h1>
+
+<p style="font-size:18px; color:#555; text-align:center;">
     Track your spending.<br>Grow your savings.
 </p>
 """, unsafe_allow_html=True)
 
-
-# Button
+# Get Started Button
 if st.button("Get Started"):
     st.switch_page("pages/home_dashboard.py")
