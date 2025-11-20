0684c5cc0fa89560b021eefeb13387dbf4b45ca2
rupasreesaladi <rupasreesaladi@gmail.com>
2025-11-20 20:40:54 +0530

    Update welcomescreen.py

--- welcomescreen.py
+++ welcomescreen.py
@@ -4,17 +4,14 @@ import streamlit as st
 st.set_page_config(page_title="Smart Expense", layout="centered")
 
 # Center content
-st.markdown("""
-    <div style="text-align: center; padding-top: 50px;">
-        <img src="https://cdn-icons-png.flaticon.com/512/2331/2331949.png" 
-             width="100" style="margin-bottom: 20px;" />
 
-        <h1 style="color:#2C3E50; font-size: 40px;">Smart Expense</h1>
-        <p style="font-size:18px; color:#555;">
-            Track your spending.<br>Grow your savings.
-        </p>
-    </div>
+st.markdown("""
+<h1 style="color:#2C3E50; font-size: 40px;">Smart Expense</h1>
+<p style="font-size:18px; color:#555;">
+    Track your spending.<br>Grow your savings.
+</p>
 """, unsafe_allow_html=True)
+
 
 # Button
 if st.button("Get Started"):
