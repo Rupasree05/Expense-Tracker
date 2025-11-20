2d9ccf7a10314f6b0b5d673ba7368c60daaf8ff8
rupasreesaladi <rupasreesaladi@gmail.com>
2025-11-20 21:18:33 +0530

    Update welcomescreen.py

--- welcomescreen.py
+++ welcomescreen.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Smart Expense", layout="centered")
 
 # Show your custom image
-st.image("C:/Users/User/Desktop/Rupa Sree/expense_Tracker_Image.png")
+st.image("C:/UsersUser/Desktop/generated-image.png")
 
 # Text content
 st.markdown("""
